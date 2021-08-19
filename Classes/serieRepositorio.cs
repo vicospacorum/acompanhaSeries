644791a7a004a656b731ea35a0245ee8f1ad3310
vicospacorum <fdomingos@usp.br>
2021-08-19 18:54:50 -0300

    Aula 3

---
+++ Classes/serieRepositorio.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using acompanhaSeries.Interfaces;
+
+namespace acompanhaSeries
+{
+    public class serieRepositorio : IRepositorio<Serie>
+    {
+        public void Atualiza(int id, Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exclui(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insere(Serie entidade)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Serie> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ProximoId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
