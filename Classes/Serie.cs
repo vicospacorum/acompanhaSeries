0903d07959b37a01e46a8559091ea618d803548a
vicospacorum <fdomingos@usp.br>
2021-08-19 19:26:17 -0300

    Classes

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -9,6 +9,7 @@ namespace acompanhaSeries
         private string Titulo {get; set;}
         private string Descricao {get; set;}
         private int Ano {get; set;}
+        private bool Excluido { get; set; }
 
         // Metodos
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
@@ -17,6 +18,7 @@ namespace acompanhaSeries
             this.Genero = genero;
             this.Titulo = titulo;
             this.Ano = ano;
+            this.Excluido = false;
         }
 
         public override string ToString()
@@ -38,6 +40,11 @@ namespace acompanhaSeries
         public int retornaId()
         {
             return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
         }
     }
 }
